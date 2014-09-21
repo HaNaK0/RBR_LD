4ced03f5f416dc0cf6677008b943646b0b27538d
HaNaK0 <saturn-v@hotmail.com>
2014-09-21 22:58:46 +0200

    quick fixes
    added som fixes like adding a new main class to be able to change out
    the whole workspace

--- RBR_LD/src/Main/__init__.py
+++ RBR_LD/src/Main/__init__.py
@@ -5,13 +5,38 @@ Created on 12 aug 2014
 '''
 from tkinter import *
 import widgets
-import workspace
+import workspace as ws
+import main
+
+main = main.Main()
 
 
-root = Tk()
-images = workspace.loadImages()
-workspace = workspace.Workspace(root, widgets.StatusBar(root, images), images, gridX = 32, gridY = 32, width = 1920 * 3, height = 1080)
-root.config(menu = widgets.MenuBar(root, workspace, images))
-root.geometry("600x600")
-root.title("RBR_LD")
-root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#workspace = None #ws.Workspace(root, widgets.StatusBar(root, images), images, gridX = 32, gridY = 32, width = 1920 * 3, height = 1080)
+#def newWorkSpace():
+    #if workspace == None:
+        #workspace = ws.Workspace(root, widgets.StatusBar(root, images), images, gridX = 32, gridY = 32, width = 1920 * 3, height = 1080)
+        #root.config(menu = widgets.MenuBar(root, workspace, images, newWorkSpace))
+
+
+
+#root = Tk()
+#images = ws.loadImages()
+
+#root.config(menu = widgets.MenuBarC(root, images, newWorkSpace))
+#root.geometry("600x600")
+#root.title("RBR_LD")
+#root.mainloop()
